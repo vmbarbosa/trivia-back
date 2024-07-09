aef206a2ee63943c6f29dae6d99289d2be739a3f
olondono <olonodono>
2024-07-09 10:32:52 -0500

    Se agrega validación de token en  el endpoint de actualización de usuario.

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import update from "../controllers/user.controller.js";
+import { valid_token } from '../services/middleware/valid-token.js'
 
 const user_router = Router()
 
-user_router.put('/update/:user_id', update)
-
+user_router.put('/update/:user_id', valid_token, update)
 
 export default user_router
