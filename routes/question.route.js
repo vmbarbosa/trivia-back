f45eac83401eb93a5b2312e5ea40d2a500ef6136
gmmonsalve <gbrielamonsalve@gmail.com>
2024-07-10 18:24:23 -0500

    Add result route

--- routes/question.route.js
+++ routes/question.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { create, create_all, update, question } from '../controllers/question.controller.js';
-import {valid_token, valid_token_get_question} from "../services/middleware/valid-token.js"
+import { result } from '../controllers/result.controller.js'
+import { valid_token, valid_token_get_question } from "../services/middleware/valid-token.js"
+import { validate_auth } from "../services/middleware/validate-auth.js";
 
 const question_router = Router()
 
@@ -11,5 +13,9 @@ question_router.post('/create', valid_token, create)
 question_router.post('/create-all', valid_token, create_all)
 
 question_router.put('/update/:id', valid_token, update)
+
+question_router.put('/update/:id', valid_token, update)
+
+question_router.post('/result/:id', validate_auth, result)
 
 export default question_router
